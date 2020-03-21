f7bdaad40c11b6450f870ec59506464e82c405d0
Aloisio Dourado <aloisio.dourado@gmail.com>
2020-03-20 23:21:06 -0300

    Alterando pagina inicial

--- App.js
+++ App.js
@@ -1,113 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, ScrollView, View, Text} from 'react-native';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <ScrollView style={styles.container}>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando 2</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando 3</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando 2</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}>Estudando 2</Text>
+        <Text style={styles.postDescription}>
+          Ips Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nam,
+          non rem illum quibusdam id amet facilis, ea, in quaerat labore dolorem
+          porro repellat eveniet eligendi sequi cum maxime impedit?
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    /*     justifyContent: 'center',
+    alignItems: 'center',
+ */ backgroundColor:
+      '#333',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  postContainer: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    padding: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 3,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  postTitle: {
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  postDescription: {
+    color: '#888',
   },
 });
 
